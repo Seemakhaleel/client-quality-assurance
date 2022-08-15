1a696f930bd2023bcfbc8e8d059ade0c561c641a
Seema <seemakhalil84@gmail.com>
2022-08-15 10:33:13 +0300

    pagination created

--- react-app/src/Pages/Questions/Questions.jsx
+++ react-app/src/Pages/Questions/Questions.jsx
@@ -1,25 +1,54 @@
 import React, { useEffect } from 'react'
 import { useState } from 'react'
 import Cards from '../../components/Cards'
-
 import Alert from './Alert'
 import { Container } from '@mui/system'
-import { Button, Grid } from '@mui/material'
+import { Button, createTheme, Grid, Pagination, ThemeProvider } from '@mui/material'
 import axiosInstance from '../../axios'
 import { baseUrl } from '../../api'
 import axios from '../../axios'
-// import { getQuestions } from '../../../src/Service'
-
 import Typography from '@mui/material/Typography'
+import { useTranslation } from 'react-i18next'
+const limit = 4 //number of questions to show per page, we have 8 questions so 8/2 = 4 questions per page and we have 4 pages
+const getTotalNumberOfPages = (dataLength) => Math.ceil(dataLength / limit) //dataLength is the length of the array of questions objects which rn is 8
+
+const paginateList = (unPaginatedList, page, limit) => {
+    const paginatedList = [] //array of questions objects that are paginated
+    const startingItemIndex = (page - 1) * limit //startingItemIndex is the index of the first item in the paginated list
+    const endingItemIndex = startingItemIndex + (limit - 1)
+
+    for (let i = startingItemIndex; i < unPaginatedList.length; i++) {
+        paginatedList.push(unPaginatedList[i])
+        if (i === endingItemIndex) break
+    }
+
+    return paginatedList ///returns an array of questions objects that are paginated
+}
 
 const Questions = () => {
     const [openPopup, setOpenPopup] = useState(false)
-    const [question, setQuestion] = useState('')
     const [description, setDescription] = useState('')
-    const [categoryId, setCategoryId] = useState('')
-    const [isClosed, setIsClosed] = useState(false)
-    const [hasBestAnswer, setHasBestAnswer] = useState(false)
     const [questions, setQuestions] = useState([])
+    const [visibleQuestions, setVisibleQuestions] = useState([])
+    const [page, setPage] = useState(1)
+    const { t } = useTranslation()
+    const theme = createTheme({
+        components: {
+            MuiButton: {
+                styleOverrides: {
+                    root: {
+                        borderRadius: 6,
+
+                        '&:hover': {
+                            backgroundColor: ' #80d8ff',
+                            color: '#00'
+                        },
+                        backgroundColor: ' #212227'
+                    }
+                }
+            }
+        }
+    })
 
     const getQuestions = async () => {
         try {
@@ -27,8 +56,9 @@ const Questions = () => {
                 method: 'get',
                 url: baseUrl + '/questions'
             })
-            console.log(response.data.questions)
+            // console.log(response.data.questions)
             setQuestions(response.data.questions) //response.data.questions is an array of questions objects with the properties of questionId, question, and answers
+            setVisibleQuestions(paginateList(response.data.questions, page, limit)) // paginateList is a function that takes an array of questions objects and returns an array of questions objects that are paginated
         } catch (error) {
             console.log('error')
         }
@@ -66,24 +96,38 @@ const Questions = () => {
 
     return (
         <>
-            <Container sx={{ my: 12, p: 2, mx: 30 }}>
+            <Container sx={{ mx: 30, marginTop: 8 }}>
                 <Grid container>
-                    <Grid item xs={12} sx={{ marginTop: 2 }}>
+                    <Grid item xs={12}>
                         <Typography variant="h5" component="h1">
-                            Questions
+                            {t('dashboard.Questions')}
                         </Typography>
-                        <Button
-                            sx={{ float: 'right' }}
-                            variant="contained"
-                            onClick={() => {
-                                setOpenPopup(true)
-                            }}
-                        >
-                            Ask a Question
-                        </Button>
+                        <ThemeProvider theme={theme}>
+                            <Button
+                                sx={{ float: 'right' }}
+                                variant="contained"
+                                onClick={() => {
+                                    setOpenPopup(true)
+                                }}
+                            >
+                                {t('dashboard.AskQuestion')}
+                            </Button>
+                        </ThemeProvider>
                     </Grid>
                     <Grid item xs={12}>
-                        <Cards questions={questions} description={description} />
+                        <Cards questions={visibleQuestions} description={description} />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Pagination
+                            page={page}
+                            onChange={(event, page) => {
+                                console.log('page', page)
+                                setPage(page)
+                                setVisibleQuestions(paginateList(questions, page, limit))
+                            }}
+                            color="primary"
+                            count={getTotalNumberOfPages(questions.length)}
+                        />
                     </Grid>
                 </Grid>
             </Container>
